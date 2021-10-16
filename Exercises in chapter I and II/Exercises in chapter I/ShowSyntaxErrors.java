fac13b51c8d9c47111790094e573ae5dfd7bb1ae
divineer <86962350+divineer@users.noreply.github.com>
2021-10-16 11:43:31 +0800

    There are exercises in chapter I
    They are out of order

---
+++ Exercises in chapter I and II/Exercises in chapter I/ShowSyntaxErrors.java
@@ -0,0 +1,10 @@
+package Chapter1;
+
+public class ShowSyntaxErrors 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to Java");
+	}
+}
+//此为程序清单1-4,看下写错程序出来的错误报告  原始是另一个双引号忘记打
---
+++ Exercises in chapter I and II/Exercises in chapter I/WelcomeWithThreeMessages.java
@@ -0,0 +1,12 @@
+package Chapter1;
+
+public class WelcomeWithThreeMessages 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Programming is fun!");
+		System.out.println("Fundamentals First");
+		System.out.println("Problem Driven");
+	}
+}
+//此为书上例题（程序清单）1-2，试着输出三个语句
